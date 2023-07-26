9e81f90b58c8a6ff7955003ec7e17e615512b359
Carlos metaute hoyos <carlosmet9409@gmail.com>
2023-07-26 00:59:24 -0500

    third another nth commit

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -6,11 +6,9 @@ const CategoryCard = ({logo, title}) => {
   const categorieName = title.replace(/\s/g, '')
   return (
     <Link href={`/categorias/${categorieName}`}>
-        <div className='flex flex-col items-center w-[125px] lg:w-[90px] xl:w-[115px]'>
-          <div className='rounded-lg border border-gray-400 grid place-items-center w-full h-24'>
-              <div className='w-[65%] h-[65%]'>
-                <Image src={logo} alt="categoria" layout='responsive' width={1} height={1} className='' />
-              </div>
+        <div className='flex flex-col items-center w-[125px] '>
+          <div className='rounded-lg border border-gray-400 grid place-items-center w-full h-24'>              
+                <img src={logo} className='h-20' alt="" />           
           </div>
           <p className='text-center h-10'>{title}</p>
         </div>
